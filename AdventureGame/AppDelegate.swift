c8b904c0f5c047929a5e69df28c2081027fd992e
Gray <13311300131@189.cn>
2019-08-12 12:09:13 +0800

    save store when the app begin background and be kill"

--- AdventureGame/AppDelegate.swift
+++ AdventureGame/AppDelegate.swift
@@ -29,6 +29,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        UIApplication.shared.beginBackgroundTask {
+            StoreDBManager.shared.saveAllStoreAndTotal()
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
