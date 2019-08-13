8592c444ac72af3d19a599c7284098b9d774da8a
Gray <13311300131@189.cn>
2019-08-13 11:30:11 +0800

    prepare data after app become active

--- AdventureGame/AppDelegate.swift
+++ AdventureGame/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        StoreManager.shared.prepareListFromDB()
+        
         return true
     }
 
@@ -41,6 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        StoreManager.shared.prepareListFromDB()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
